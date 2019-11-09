94ca6af51286c6d9075bda630835f7e5a120d050
unknown <bala.andreea@yahoo.com>
2019-11-09 14:43:56 -0800

    [master] Add vue cli with ts & pug & stylus.

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  #app
+    router-view
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({})
+export default class App extends Vue {
+  /**
+   * Class constructor.
+   */
+  // constructor() {
+  //   super();
+  // }
+}
+</script>
+
+<style lang="stylus">
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  .home
+    h1 Home
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+export default class Home extends Vue {
+  /**
+   * Class constructor.
+   */
+  // constructor() {
+  //   super();
+  // }
+}
+</script>
