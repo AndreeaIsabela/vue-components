b8767de7676f105c301d0c8be10ed8de9d5c51a2
AndreeaIsabela <bala.andreea@yahoo.com>
2020-08-24 23:16:12 +0300

    [master] Add pagination view.

---
+++ src/views/PaginationView.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  div.offset-1
+    Pagination(:totalPages="totalPages" :maxVisibleButtons="3")
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+import Pagination from '../components/pagination/Pagination.vue';
+
+@Component({
+  components: {
+    Pagination
+  },
+})
+export default class PaginationView extends Vue {
+
+  public totalPages: number = 10;
+  /**
+   * Class constructor.
+   */
+  constructor() {
+    super();
+  }
+
+}
+</script>
+
+<style lang="stylus" scoped>
+  .pagination
+    margin-top: 80px
+</style>
